5a8cef4500b7afc4deec9e2bbe9ae1949db0dc51
Trinh Quan <trinhquan.171093@gmail.com>
2017-03-28 11:02:27 +0700

    add collection view extension

---
+++ Classes/UICollectionView+Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  UICollectionView+Extensions.swift
+//  JKEssential
+//
+//  Created by Trịnh Quân on 3/28/17.
+//  Copyright © 2017 Trịnh Quân. All rights reserved.
+//
+
+import UIKit
+
+public extension UICollectionView {
+    
+    public func registerCellClass(cellClass: AnyClass) {
+        let identifier = NSStringFromClass(cellClass).components(separatedBy: ".").last!
+        self.register(cellClass, forCellWithReuseIdentifier: identifier)
+    }
+    
+    public func registerCellNib(cellClass: AnyClass) {
+        let identifier = NSStringFromClass(cellClass).components(separatedBy: ".").last!
+        let nib = UINib(nibName: identifier, bundle: Bundle.main)
+        self.register(nib, forCellWithReuseIdentifier: identifier)
+    }
+}
--- JKEssential.podspec
+++ JKEssential.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |s|
   #
 
   s.name         = "JKEssential"
-  s.version      = "0.0.3"
+  s.version      = "0.0.4"
   s.summary      = "Some helpful data model and method for all swift iOS project"
 
   # This description is used to generate tags and improve search results.
@@ -78,7 +78,7 @@ Pod::Spec.new do |s|
   #  Supports git, hg, bzr, svn and HTTP.
   #
 
-  s.source       = { :git => "https://github.com/jarklee/JKEssential.git", :tag => "0.0.3" }
+  s.source       = { :git => "https://github.com/jarklee/JKEssential.git", :tag => "0.0.4" }
 
 
   # ――― Source Code ―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――― #
